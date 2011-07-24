a028b02f181e8868221677609b829bacbfb9cd38
Mike Megally <git@cmsimike.com>
2011-07-24 14:30:42 -0700

    uncommented the positivebigintegerclass as there is no reason it should be commented out. back to using positivebiginteger as i shouldn't resume south usage. you can extend the mapping class in your own project and figure out how to tell south to use it yourself, like i am about to do. also got rid of a project-specific term.

--- twitter_auth/models.py
+++ twitter_auth/models.py
@@ -3,19 +3,19 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
 # Based on http://www.numlock.ch/news/it/django-custom-model-field-for-an-unsigned-bigint-data-type/
-#class PositiveBigIntegerField(models.PositiveIntegerField):
-#    """Represents MySQL's unsigned BIGINT data type (works with MySQL only!)"""
-#    empty_strings_allowed = False
-#
-#    def get_internal_type(self):
-#        return "PositiveBigIntegerField"
-#
-#    def db_type(self, connection):
-#        # This is how MySQL defines 64 bit unsigned integer data types
-#        return "bigint UNSIGNED"
+class PositiveBigIntegerField(models.PositiveIntegerField):
+    """Represents MySQL's unsigned BIGINT data type (works with MySQL only!)"""
+    empty_strings_allowed = False
+
+    def get_internal_type(self):
+        return "PositiveBigIntegerField"
+
+    def db_type(self, connection):
+        # This is how MySQL defines 64 bit unsigned integer data types
+        return "bigint UNSIGNED"
 
 class MapTwitterToUser(models.Model):
-    twitter_id  = models.BigIntegerField(null=False, blank=False)
+    twitter_id  = PositiveBigIntegerField(null=False, blank=False)
     user        = models.ForeignKey(User)
   
     def __unicode__(self):
@@ -25,7 +25,7 @@ class MapTwitterToUser(models.Model):
         #ordering = ("-reverse_rank",)
         app_label = "twitter_auth"
         verbose_name = "maptwittertouser"
-        verbose_name_plural = "maptwitteruserstotwitdegreeusers"
+        verbose_name_plural = "maptwittertousers"
 
 class TwitterAuthUserProfile(models.Model):
     user                = models.ForeignKey(User, unique=True)

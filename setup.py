1abd6065afafc351bc52dd8f8d325335a47bb8fb
James Morris <james@jmoz.co.uk>
2012-11-15 13:44:10 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='django-twitter-oauth',
+    version='1.0',
+    url='https://github.com/jmoz/django-twitter-oauth',
+    author='Mike Megally',
+    author_email='dylan@dyve.net',
+    packages=['python-twitter'],
+    include_package_data=True,
+    description='django-twitter-oauth (forked from http://github.com/henriklied/django-twitter-oauth)',
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ],
+)
